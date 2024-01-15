03d44a39c25e432fb308908ae5f189e86bc3df6c
CodeBlogSpot <coderblogspot@gmail.com>
2024-01-15 12:10:00 +0200

    upload some files

---
+++ src/const.py
@@ -0,0 +1,8 @@
+# screen dimensions
+WIDTH = 630
+HEIGHT = 630
+
+# Board dimensions
+COLS = 8
+ROWS = 8
+SQSIZE = WIDTH // COLS
---
+++ src/square.py
@@ -0,0 +1,12 @@
+import pygame
+from const import *
+
+
+class Square:
+    def __init__(self, row, col, piece=None):
+        self.row = row
+        self.col = col
+        self.piece = piece
+
+    def has_piece(self):
+        return self.piece != None
